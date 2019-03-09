be15c65c0fbbc5178375219f0ead3b6e5217a97b
Ian Mubangizi <io@ianmubangizi.com>
2019-03-09 21:03:43 +0200

    Created Project Files with pom.xml, Added to Git Version Control, Made Main.java with main

---
+++ src/main/java/com/ianmubangizi/electronic_invoice_entry/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ianmubangizi.electronic_invoice_entry;
+
+/**
+ *
+ * @author perso
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
