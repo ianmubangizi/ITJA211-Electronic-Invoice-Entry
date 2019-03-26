a8bbba5eb99be0b6c925ff376953170a228a1c3a
Ian Mubangizi <io@ianmubangizi.com>
2019-03-26 17:52:41 +0200

    Hard Refactor of classes, Now making use of Singletons for Frames, Had a problematic actionPerformed being called twice per single click

--- src/main/java/com/electronic_invoice/Utils/Helpers.java
+++ src/main/java/com/electronic_invoice/Utils/Helpers.java
@@ -30,9 +30,42 @@ public class Helpers {
     }
 
     //
+    public void addCustomer(ECallTypes t) {
+        switch (t) {
+            case ADD_CUSTOMER:
+                new ClientAction().addCustomer(
+                        InvoiceEntry.getTransactionFrame()
+                );
+                break;
+            case CUSTOMERID_TAKEN:
+                new MessagePane(null,
+                        "The ", "Adding Customer Error", MessagePane.EMessage.ERROR);
+                break;
+            case NEED_VAILD_CUSTOMERID:
+                new MessagePane(null,
+                        "Can't Create Invoice without a vaild Customer Number\n"
+                        + "Press Okay and Enter Customer Info to get a Number\n"
+                        + "Click the Add Customer Button after inputting Customer Info",
+                        "Adding Invoice Error", MessagePane.EMessage.ERROR);
+                break;
+            case ADD_INVOICE_CUSTOMER:
+                new MessagePane(null,
+                        "Enter your Customer Details then\n"
+                        + "Click the Add Customer Button then\n"
+                        + "Try Adding an Invoice",
+                        "Need a Customer Number - When Adding Invoice",
+                        MessagePane.EMessage.INFO);
+                break;
+            default:
+                break;
+        }
+    }
+
+    //
     public void displayProductList(InvoiceEntry ief) {
         new FindProduct().allProducts().forEach((product) -> {
-            ief.jcbx_allproducts.addItem(product.getDescription());
+            ief.jcbx_allproducts.addItem(product.getDescription() + "  [Code:  "
+                    + product.getProduct_code() + "]");
         });
     }
 
@@ -53,11 +86,19 @@ public class Helpers {
     }
 
     //
-    public void displayError(Exception e) {
+    public void displayErrors(Exception e) {
+        String msg = "";
         if (NumberFormatException.class.isInstance(e)) {
+            switch (e.getMessage()) {
+                case "For input string: \"\"":
+                case "empty String":
+                    msg = "Field Has Empty Value";
+                    break;
+                default:
+                    break;
+            }
             new MessagePane(null, "Please Input Correct values into Form fields",
-                    "Invaild Inputs [Empty Values]", MessagePane.EMessage.ERROR);
+                    "Invaild Input [" + msg + "]", MessagePane.EMessage.ERROR);
         }
-        System.out.println(e.getMessage());
     }
 }
